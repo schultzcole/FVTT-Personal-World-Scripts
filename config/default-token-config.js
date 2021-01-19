60021164fb58dee073b5e15c22ab8beabbfb3f68
Cole Schultz <schultzcole@users.noreply.github.com>
2021-01-19 02:45:43 -0600

    Update default-token-config.js
    Improve formatting, comments, and general cleanup

--- config/default-token-config.js
+++ config/default-token-config.js
@@ -1,32 +1,33 @@
-// This script will change the default token configuration for newly created or imported actors
-//   (does not affect actors already in the world).
-// It changes the following:
-// - Show the token's name when an owner of the token hovers over it
-// - Always show all resource bars for owners of the token
-// - Sets the first bar to display the token's hit points (dnd5e)
+/**
+ * @author cole#9640
+ * This script will change the default token configuration. It changes the following:
+ *  - If the actor is a PC, show the name for all players. Otherwise, show it to the token's owner when hovered.
+ *  - Always show all resource bars for owners of the token.
+ *  - Set the first bar to display the token's hit points (dnd5e).
+ *  - Set the second bar to display nothing.
+ */
+
+// This hook sets the default token configuration for new actors that are created
 Hooks.on("preCreateActor", (actorData, options, userId) => {
-  mergeObject(actorData.token, createTokenData(actorData));
+    mergeObject(actorData.token, createDefaultTokenData(actorData.type));
 });
 
-function createTokenData(actorData) {
-  const tokenData = actorData.token ?? {};
-  
-  const display = actorData.type === "character"
-    ? CONST.TOKEN_DISPLAY_MODES.OWNER
-    : CONST.TOKEN_DISPLAY_MODES.OWNER_HOVER;
-  
-  return {
-    displayName: display,
-    displayBars: display,
-    bar1: { attribute: "attributes.hp" },
-    bar2: { attribute: null },
-  };
+// This function is what defines the token configuration we would like to be the default
+function createDefaultTokenData(type) {
+    const displayName = type === "character" ? CONST.TOKEN_DISPLAY_MODES.ALWAYS : CONST.TOKEN_DISPLAY_MODES.OWNER_HOVER;
+
+    return {
+        displayName,
+        displayBars: CONST.TOKEN_DISPLAY_MODES.OWNER,
+        bar1: { attribute: "attributes.hp" },
+        bar2: { attribute: null },
+    };
 }
 
 // This function can be called via macro to convert existing world actors to use the correct prototype
 // token settings. This will not affect tokens already placed in scenes.
 async function convertExistingActorPrototypeTokens() {
-  return game.actors.updateAll(a => ({ token: createTokenData(a.data) }));
+    return game.actors.updateAll((a) => ({ token: createDefaultTokenData(a.data.type) }));
 }
 
 globalThis.convertExistingActorPrototypeTokens = convertExistingActorPrototypeTokens;
